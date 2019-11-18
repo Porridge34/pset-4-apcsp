331964bc3d2e8c9427a017b33b2ad2f0ce58aa91
Ivy Ng <INg@ucvts.tec.nj.us>
2019-11-18 10:50:21 -0500

    joe mama haha gottem

--- src/prime.js
+++ src/prime.js
@@ -1,14 +1,14 @@
 const input = require("readline-sync");
-let integerEntered = Number(input.question("\nNon-negative: "));
-let sum = 0;
-while (integerEntered > Number.MAX_SAFE_INTEGER || integerEntered < Number.MIN_SAFE_INTEGER || Number.isNaN(integerEntered) == true || integerEntered%1!=0){
-  integerEntered = Number(input.question("Non-negative: "));
+let integerEntered = Number(input.question("\nNon-negative integer: "));
+let prime = "Prime";
+while (integerEntered > Number.MAX_SAFE_INTEGER || integerEntered < 0 || Number.isNaN(integerEntered) == true || integerEntered%1!=0){
+  integerEntered = Number(input.question("Non-negative integer: "));
 }
 for(var i = 2; i < integerEntered; i++){
   if(integerEntered % i === 0){
-
+    prime = "Not prime";
+  }else if(integerEntered == 1 || integerEntered == 2){
+    prime = "Prime";
   }
 }
-
-
-console.log("\n"+sum+".");
+console.log("\n"+prime+".");
